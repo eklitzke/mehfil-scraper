f0b9df1acd7d027dab184ec49970cee5a01dda1d
Evan Klitzke <evan@dhcp-10-10-7-101.yelpcorp.com>
2008-03-12 11:00:28 -0700

    add the mehfil.py scraper script

---
+++ mehfil.py
@@ -0,0 +1,28 @@
+import urllib
+import re
+import sys
+
+tag_re = re.compile('<.*?>')
+num_re = re.compile(r'\(\d\)')
+
+if __name__ == '__main__':
+	u = urllib.urlopen('http://mehfilindian.com/LunchMenuTakeOut.htm').read()
+	s = u.find('height="539"')
+	u = u[s:s+u[s:].find('<center>')]
+	u = tag_re.sub('', u.replace('\n', '').replace('\r', '').replace('\t', ' ').replace('<br>','\n').replace('  ',' '))
+
+	v = u
+
+	positions = [-3]
+	while u:
+		m = num_re.search(u)
+		if not m:
+			break
+		positions.append(m.start() + positions[-1] + 3)
+		u = u[m.end():]
+	positions = positions[1:]
+
+	for x, y in zip(positions, positions[1:]):
+		print v[x:y].strip().decode('windows-1252')
+		print
+	print '(%s)%s' % (len(positions), u)
